d417e87cbd4471047b1a121e1bcd23b1a5f34cd1
Jamie <jamieefung@gmail.com>
2017-10-30 23:40:53 -0400

    added login template and route

--- cbapp/routes.py
+++ cbapp/routes.py
@@ -5,3 +5,7 @@ from cbapp import app
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
