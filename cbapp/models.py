be5a548e10f8000ab6bbe4c102090b791a264899
Ahalya Sanjiv <ahalyasanjiv@gmail.com>
2017-10-31 05:06:07 -0400

    Added password checking function

--- cbapp/models.py
+++ cbapp/models.py
@@ -6,14 +6,14 @@ db = SQLAlchemy()
 
 class User(db.Model):
   """
-  Chorus Battle User Class
+  Chorus battle user class
   """
   __tablename__ = 'users'
   user_id = db.Column(db.Integer, primary_key = True)
   firstname = db.Column(db.String(100))
   lastname = db.Column(db.String(100))
   email = db.Column(db.String(120), unique=True)
-  password = db.Column(db.String(54))
+  password_hash = db.Column(db.String(54))
 
   def __init__(self, firstname, lastname, email, password):
     self.firstname = firstname.title()
@@ -28,4 +28,16 @@ class User(db.Model):
   Args:
     password (str): new password to replace current password
   """
-    self.password = generate_password_hash(password)
+    self.password_hash = generate_password_hash(password)
+
+  def check_password(self, password):
+  """
+  Checks if password matches hashed password
+
+  Args:
+    password (str): password to be checked
+
+  Returns:
+    bool: True if password matches hashed password and false if not
+  """  
+    return check_password_hash(self.password_hash,password)
