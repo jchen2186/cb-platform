01040a56004529d413ca4be77c63b7fc14d53ac4
Ahalya Sanjiv <ahalyasanjiv@gmail.com>
2017-10-31 04:48:58 -0400

    Added connection to database

--- cbapp/__init__.py
+++ cbapp/__init__.py
@@ -1,3 +1,4 @@
 from flask import Flask
 app = Flask(__name__)       # pylint: disable=invalid-name
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/cbplatform'
 import cbapp.routes    # pylint: disable=wrong-import-position
