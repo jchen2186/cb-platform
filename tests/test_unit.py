783fa34f207314f6fc42566c9aa0972c41f1f4f6
Jane Chen <jane.chen@macaulay.cuny.edu>
2017-10-31 14:20:03 -0400

    added login and signup but it does not function yet

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,5 +1,6 @@
 import unittest
 import cbapp
+
 from unittest.mock import patch, DEFAULT, Mock
 
 class TestCBAppUnit(unittest.TestCase):
@@ -21,4 +22,23 @@ class TestCBAppUnit(unittest.TestCase):
 		template_filename = call_args[0][0]
 		self.assertEqual(template_name, "index.html")
 
-	def test_index_successful_render(self):
+	# def test_index_successful_render(self):
+
+# from unittest.mock import patch, DEFAULT, mock
+
+# class TestCBAppUnit(unittest.TestCase):
+
+# 	#checks login route
+# 	def test_login_gets_login_template(self):
+# 		with patch.multiple("cbapp.routes",
+# 							request=DEFAULT,
+# 							render_template=DEFAULT) as mock_funcs:
+# 			cbapps.routes.login()
+# 			render_template = mock_funcs["render_template"]
+# 			#makes sure we are rendering a template on the login route
+# 			self.assertTrue(render_template.called)
+# 			calls_args = render_template.call_args
+# 			file_name = call_args[0][0]
+# 			#makes sure we are rendering the correct template on the login route
+# 			self.assertEqual(file_name, "login.html")
+
