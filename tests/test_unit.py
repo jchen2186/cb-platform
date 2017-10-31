2a8e0da0ab9a8345a123bbbb3902036395e5b77a
Vincent Yu <vinjyu1@gmail.com>
2017-10-31 13:46:29 -0400

    comments for testing login template

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -4,13 +4,16 @@ from unittest.mock import patch, DEFAULT, mock
 
 class TestCBAppUnit(unittest.TestCase):
 
+	#checks login route
 	def test_login_gets_login_template(self):
 		with patch.multiple("cbapp.routes",
 							request=DEFAULT,
 							render_template=DEFAULT) as mock_funcs:
 			cbapps.routes.login()
 			render_template = mock_funcs["render_template"]
+			#makes sure we are rendering a template on the login route
 			self.assertTrue(render_template.called)
 			calls_args = render_template.call_args
 			file_name = call_args[0][0]
+			#makes sure we are rendering the correct template on the login route
 			self.assertEqual(file_name, "login.html")
