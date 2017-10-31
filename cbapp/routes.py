93d405f9a16c6f47532c2fb9dc0032ce1bc590f3
Jamie <jamieefung@gmail.com>
2017-10-31 01:15:48 -0400

    added chorus battle info page template

--- cbapp/routes.py
+++ cbapp/routes.py
@@ -9,3 +9,7 @@ def index():
 @app.route('/login', methods=['GET'])
 def login():
     return render_template('login.html')
+
+@app.route('/chorusinfo/<cb>', methods=['GET'])
+def chorusInfo(cb=None):
+    return render_template('chorusinfo.html', chorusTitle=cb)
