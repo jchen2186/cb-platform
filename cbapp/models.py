671098e90ea4d2b1da2fa73de834906843d95f34
Jane Chen <jane.chen@macaulay.cuny.edu>
2017-11-05 12:41:28 -0500

    signup functions, but had to comment out most of the models

--- cbapp/models.py
+++ cbapp/models.py
@@ -3,7 +3,6 @@ from werkzeug import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
-
 class User(db.Model):
     """
     Chorus battle user class
@@ -13,12 +12,12 @@ class User(db.Model):
     firstname = db.Column(db.String(100))
     lastname = db.Column(db.String(100))
     email = db.Column(db.String(120), unique=True)
-    password_hash = db.Column(db.String(54))
-    username = db.Column(db.String(100))
-    role = db.Column(db.Integer, db.ForeignKey('userroles.id'))
+    password_hash = db.Column(db.String(100))
+    username = db.Column(db.String(100), unique=True)
+    # role = db.Column(db.Integer, db.ForeignKey('userroles.id'))
 
 
-    def __init__(self, firstname, lastname, email, password,username,role):
+    def __init__(self, firstname, lastname, email, password, username, role):
         self.firstname = firstname.title()
         self.lastname = lastname.title()
         self.email = email.lower()
@@ -45,51 +44,50 @@ class User(db.Model):
         Returns:
             bool: True if password matches hashed password and false if not
         """    
-        return check_password_hash(self.password_hash,password)
+        return check_password_hash(self.password_hash, password)
 
+# class ChorusBattle(db.Model):
+#     """
+#     Chorus battle class
+#     """
+#     __tablename__ = 'chorusbattles'
+#     id = db.Column(db.Integer, primary_key = True)
+#     name = db.Column(db.String(150))
+#     organizers = db.relationship(
+#         'User', 
+#         backref='chorusbattle', 
+#         lazy='dynamic'
+#     )
+#     entries = db.relationship(
+#         'Entry', 
+#         backref='chorusbattle', 
+#         lazy='dynamic'
+#     )
 
-class ChorusBattle(db.Model):
-    """
-    Chorus battle class
-    """
-    __tablename__ = 'chorusbattles'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(150))
-    organizers = db.relationship(
-        'User', 
-        backref='chorusbattle', 
-        lazy='dynamic'
-    )
-    entries = db.relationship(
-        'Entry', 
-        backref='chorusbattle', 
-        lazy='dynamic'
-    )
+#     def __init__(self,title):
+#         self.name = name
 
-    def __init__(self,title):
-        self.name = name
+# class UserRole(db.Model):
+#     """
+#     User role class
+#     """
+#     __tablename__ = 'userroles'
+#     id = db.Column(db.Integer, primary_key = True)
+#     role_title = db.Column(db.String(100))
 
-class UserRole(db.Model):
-    """
-    User role class
-    """
-    __tablename__ = 'userroles'
-    id = db.Column(db.Integer, primary_key = True)
-    role_title = db.Column(db.String(100))
+#     def __init__(self, role_id, role_title):
+#         self.id = role_id
+#         self.role_title = role_title
 
-    def __init__(self, role_id, role_title):
-        self.id = role_id
-        self.role_title = role_title
-
-class Entry(db.Model):
-    """
-    Chorus battle entry
-    """
-    __tablename__ = 'entries'
-    id = db.Column(db.Integer, primary_key = True)
-    submission_date = db.Column(db.DateTime) 
-    owners = db.relationship(
-        'User', 
-        backref='entry', 
-        lazy='dynamic'
-    )
+# class Entry(db.Model):
+#     """
+#     Chorus battle entry
+#     """
+#     __tablename__ = 'entries'
+#     id = db.Column(db.Integer, primary_key = True)
+#     submission_date = db.Column(db.DateTime) 
+#     owners = db.relationship(
+#         'User', 
+#         backref='entry', 
+#         lazy='dynamic'
+#     )
--- cbapp/routes.py
+++ cbapp/routes.py
@@ -1,12 +1,12 @@
-from flask import render_template
-from flask import request
-from cbapp.forms import SignupForm, LoginForm
+from flask import render_template, request
+from forms import SignupForm, LoginForm
+from models import db, User #, ChorusBattle, UserRole, Entry
+from cbapp import app
 
-if __name__ != '__main__':
-	from cbapp import app
-else:
-	from flask import Flask
-	app = Flask(__name__)
+# connect app to the postgresql database (local to our machines)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/cbapp'
+db.init_app(app)
+app.secret_key = 'development-key'
 
 @app.route('/', methods=['GET'])
 def index():
@@ -27,14 +27,18 @@ def signup():
     form = SignupForm()
 
     if request.method == 'POST':
+        if form.validate() == False:
+            return render_template('signup.html', form=form)
         # temporary, change later
-        return 'Success'
+        else:
+            newuser = User(form.first_name.data, form.last_name.data, form.email.data, 
+                form.password.data, form.username.data, form.role.data)
+            db.session.add(newuser)
+            db.session.commit()
+            return 'Success'
     elif request.method == 'GET':
         return render_template('signup.html', form=form)
 
 @app.route('/chorusinfo/<cb>', methods=['GET'])
 def chorusInfo(cb=None):
     return render_template('chorusinfo.html', chorusTitle=cb)
-
-if __name__ == '__main__':
-	app.run(debug=True)
