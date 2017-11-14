9679df60ce260ea934eccd0bb6311c018232014f
jamesw8 <jameswong8@gmail.com>
2017-11-13 21:53:56 -0500

    Added port and host

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from cbapp import app
+import os
 
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    port = int(os.environ.get("PORT", 5000)
+    app.run(host='0.0.0.0', port=port)
