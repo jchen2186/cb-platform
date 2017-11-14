692afae3488553e3aed7d093d5ec184d6db8b0f6
Jane Chen <jane.chen@macaulay.cuny.edu>
2017-11-14 14:50:16 -0500

    made login and signup pages look a little bit nicer, changed role field to a select field

--- cbapp/forms.py
+++ cbapp/forms.py
@@ -3,7 +3,7 @@ This module contains the structure of all of the forms used on the app.
 """
 
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Email, Length
 
 class SignupForm(Form):
@@ -22,7 +22,10 @@ class SignupForm(Form):
         Length(min=6, message='Passwords must have at least 6 characters.')])
 
     # change this later to specify the possible roles the user can be
-    role = StringField('Role') # validators=[DataRequired('Please enter your role.')])
+    role_choices = [('organizer', 'Organizer/Judge'), ('singer', 'Singer'), 
+                    ('artist', 'Artist'), ('mixer', 'Mixer')]
+    role = SelectField('Role', choices=role_choices)
+    
     submit = SubmitField('Sign up')
 
 class LoginForm(Form):
