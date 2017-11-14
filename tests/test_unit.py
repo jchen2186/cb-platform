21135334b1e1f114975d410698aa1409d8ff99d8
Jane Chen <jane.chen@macaulay.cuny.edu>
2017-11-13 23:58:34 -0500

    added another module

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,49 +1,55 @@
+"""
+This module contains the unit tests that should be run to ensure that
+our app works as intended.
+"""
+
 #note: comment out garbage in __init__ py to run pytest without other errors
 
 import unittest
+from unittest.mock import patch, DEFAULT, Mock
 import cbapp
 
-from unittest.mock import patch, DEFAULT, Mock
-
 class TestCBAppUnit(unittest.TestCase):
+    """Class of unit tests that checks if the templates exist."""
 
-	#checks login route
-	def test_login_gets_login_template(self):
-		with patch.multiple("cbapp.routes",
-							request=DEFAULT,
-							render_template=DEFAULT) as mock_functions:
-			cbapp.routes.login()
-			render_template = mock_functions["render_template"]
-			#makes sure we are rendering a template on the login route
-			self.assertTrue(render_template.called)
-			call_args = render_template.call_args
-			file_name = call_args[0][0]
-			#makes sure we are rendering the correct template on the login route
-			self.assertEqual(file_name, "login.html")
+    def test_login_gets_login_template(self):
+        """Checks if the login route exists. The test passes if it does."""
+        with patch.multiple("cbapp.routes",
+                            request=DEFAULT,
+                            render_template=DEFAULT) as mock_functions:
+            cbapp.routes.login()
+            render_template = mock_functions["render_template"]
+            #makes sure we are rendering a template on the login route
+            self.assertTrue(render_template.called)
+            call_args = render_template.call_args
+            file_name = call_args[0][0]
+            #makes sure we are rendering the correct template on the login route
+            self.assertEqual(file_name, "login.html")
 
-	# Test for getting the index template
-	def test_index_get_index_template(self):
-		# Create mock functions
-		with patch.multiple('cbapp.routes',
-							request=DEFAULT,
-							render_template=DEFAULT) as mock_functions:
-			cbapp.routes.index()
-			# Check if render_template is called when index route is visited
-			render_template = mock_functions['render_template']
-			self.assertTrue(render_template.called)
+    def test_index_get_index_template(self):
+        """Checks if the index route exists. The test passes if it does."""
+        # Create mock functions
+        with patch.multiple('cbapp.routes',
+                            request=DEFAULT,
+                            render_template=DEFAULT) as mock_functions:
+            cbapp.routes.index()
+            # Check if render_template is called when index route is visited
+            render_template = mock_functions['render_template']
+            self.assertTrue(render_template.called)
 
-			# Check if the template rendered is index.html
-			call_args = render_template.call_args
-			filename = call_args[0][0]
-			self.assertEqual(filename, "index.html")
+            # Check if the template rendered is index.html
+            call_args = render_template.call_args
+            filename = call_args[0][0]
+            self.assertEqual(filename, "index.html")
 
-	def test_cbinfo_get_cbinfo_template(self):
-		with patch.multiple("cbapp.routes",
-							request=DEFAULT,
-							render_template=DEFAULT) as mock_functions:
-			cbapp.routes.chorusInfo()
-			render_template = mock_functions["render_template"]
-			self.assertTrue(render_template.called)
-			call_args = render_template.call_args
-			file_name = call_args[0][0]
-			self.assertEqual(file_name, "chorusinfo.html")
+    def test_cbinfo_get_cbinfo_template(self):
+        """Checks if the chorusinfo route exists. The test passes if it does."""
+        with patch.multiple("cbapp.routes",
+                            request=DEFAULT,
+                            render_template=DEFAULT) as mock_functions:
+            cbapp.routes.chorusInfo()
+            render_template = mock_functions["render_template"]
+            self.assertTrue(render_template.called)
+            call_args = render_template.call_args
+            file_name = call_args[0][0]
+            self.assertEqual(file_name, "chorusinfo.html")
