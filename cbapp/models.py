064e2aaa5f5ca244f341b9d599170b3ad8045dc1
jamesw8 <jameswong8@gmail.com>
2017-12-05 21:36:25 -0500

    add title to Entry

--- cbapp/models.py
+++ cbapp/models.py
@@ -207,14 +207,16 @@ class Entry(db.Model):
     id = db.Column(db.Integer, primary_key = True) #: Primary key to identify the round.
 
     team_id = db.Column(db.Integer, db.ForeignKey('teams.id')) #: id of the team that made the entry.
+    title = db.Column(db.String(100)) #: Title of the entry.
     description = db.Column(db.String(500)) #: User-inputted description of the entry.
     video_link= db.Column(db.String(500)) #: Link to the video entry, preferably YouTube so we can embed it.
     submission_date = db.Column(db.DateTime(timezone=True), default=func.now()) #: Date of the submission of the entry.
     chorusbattle = db.Column(db.Integer, db.ForeignKey('chorusbattles.id')) #: The chorus battle the entry belongs to
     round_number = db.Column(db.Integer, db.ForeignKey('rounds.id')) #: The round of the chorus battle that the entry belongs to.
 
-    def __init__(self, team_name, description, video_link, chorusname, round_number):
-        self.team_name = team_name
+    def __init__(self, team_name, title, description, video_link, chorusid, round_number):
+        self.team_id = Team.query.filter_by(team_name=team_name).first().id
+        self.title = title
         self.description = description
         self.video_link = video_link
         self.chorusbattle = chorusid
@@ -242,14 +244,17 @@ class Team(db.Model):
     """
     __tablename__ = 'teams'
     id = db.Column(db.Integer, primary_key = True) #: Primary key to identify the team.
-    team_name=db.Column(db.String(100)) #: Name of the team.
+    team_name = db.Column(db.String(100)) #: Name of the team.
+    leader_id = db.Column(db.String(100), db.ForeignKey('users.id')) #: User ID of team leader.
     team_logo = db.Column(db.LargeBinary) #: Image for the team logo.
-    chorusbattle = db.Column(db.Integer, db.ForeignKey('chorusbattles.id')) 
+    chorusbattle = db.Column(db.Integer, db.ForeignKey('chorusbattles.id')) #: The chorus battle the team is participating in.
     """ id of the ChorusBattle the team belongs to. A new team must be created per chorus battle, even if they have the same name and same members.
     """
 
-    def __init__(self, id, chorusbattle):
+    def __init__(self, id, team_name, leader_id, chorusbattle):
         self.id = id
+        self.team_name = team_name
+        self.leader_id = leader_id
         self.chorusbattle = chorusbattle
 
 class Judge(db.Model):
