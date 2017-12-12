52d232329d98b91c0a261d7b5068f5c31e6d10b4
Vincent Yu <vinjyu1@gmail.com>
2017-12-12 15:02:04 -0500

    test for search result page

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -434,6 +434,23 @@ class TestCBAppUnitRoutesExist(unittest.TestCase):
                 file_name = call_args[0][0]
                 self.assertEqual(file_name, "login")
 
+    def test_search_redirect_search_template(self):
+        """Checks if the home route redirects the user to login when the user is not logged in.
+        The test passes if it does."""
+        with cbapp.app.test_request_context('/search/', method='POST'):
+            self.assertEqual(flask.request.path, '/search/')
+            self.assertEqual(flask.request.method, 'POST')
+
+            with patch.multiple("cbapp.routes",
+                                request=DEFAULT,
+                                render_template=DEFAULT) as mock_functions:
+                cbapp.routes.search()
+                render_template = mock_functions["render_template"]
+
+                call_args = render_template.call_args
+                file_name = call_args[0][0]
+                self.assertEqual(file_name, "searchresult.html")
+
     # # need to fix this so it generates home when a user is logged in
     # def test_home_get_home_template(self):
     #     """Checks if the home route exists. The test passes if it does."""
